5bcf3d9c88e4b4c89e923b6060d601440d5e9299
J0EK3R <J0EK3R@users.noreply.github.com>
2018-01-15 18:01:38 +0100

    Update docker-entrypoint.sh

--- ebusd/develop/docker-entrypoint.sh
+++ ebusd/develop/docker-entrypoint.sh
@@ -80,7 +80,7 @@ fi
 # if environment variable EBUSD_LOGFILE is set then tee console output to logfile
 if [ "$EBUSD_LOGFILE" != "" ]; then
   set -- "$@" " | tee -a $EBUSD_LOGFILE"
-  echo "Starting with commandline: $@" | tee -a $EBUSD_LOGFILE
+  echo "Starting with commandline: $@" |& tee -a $EBUSD_LOGFILE
 else
   echo "Starting with commandline: $@"
 fi
